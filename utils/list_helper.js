f45b19d248d82f5c3161a9223346f6db966c5fa0
Jaakko Lind <jaakko.lind@tuni.fi>
2022-08-17 13:41:58 +0300

    Finalized exercises 4.6 and 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -9,7 +11,43 @@ const totalLikes = (blogs) => {
   return sum
 }
 
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce((max, blog) =>
+    max.likes > blog.likes ? max : blog
+  )
+  return (({ title, author, likes }) => ({ title, author, likes }))(favorite)
+}
+
+const mostBlogs = (blogs) => {
+  const authors = _.chain(blogs)
+    .groupBy('author')
+    .map((value, key) => ({ author: key, blogs: _.size(value) }))
+    .value()
+  const favorite = authors.reduce((max, author) =>
+    max.blogs > author.blogs ? max : author
+  )
+  return favorite
+}
+
+const mostLikes = (blogs) => {
+  const authors = _(blogs)
+    .groupBy('author')
+    .map((objs, key) => ({
+      author: key,
+      likes: _.sumBy(objs, 'likes'),
+    }))
+    .value()
+
+  const favorite = authors.reduce((max, author) =>
+    max.likes > author.likes ? max : author
+  )
+  return favorite
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
